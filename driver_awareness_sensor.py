e6c013636bc9d261d8e7d130d8bc13bb441ee20e
Nathan Gelderloos <ngelderloos7@gmail.com>
2013-04-09 17:55:45 -0400

    Created smooth_face()
    Implemented initial smoothing of face box

--- driver_awareness_sensor.py
+++ driver_awareness_sensor.py
@@ -1,12 +1,18 @@
 import numpy as np
 import cv2
 import cv2.cv as cv
-from video import create_capture
-from common import clock, draw_str
 import time
+import numpy as np
 
-cascade_fn = "../data/haarcascades/haarcascade_frontalface_alt.xml"
-nested_fn = "../data/haarcascades/haarcascade_eye.xml"
+cascade_fn = "./data/haarcascades/haarcascade_frontalface_alt.xml"
+nested_fn = "./data/haarcascades/haarcascade_eye.xml"
+
+SMOOTH_FACE_MAX = 10
+smooth_face_history = np.zeros((SMOOTH_FACE_MAX, 4), dtype=np.int32)
+smooth_face_counter = 0
+
+SHOW_REAL_TIME = False
+SHOW_SMOOTH = True
 
 #-------------------------------------------------------------------------
 # detect_face() uses the given cascade to find the desired features.
@@ -49,19 +55,44 @@ def draw_rects(img, rects, color):
 def draw_text(img, text, (x, y)):
     cv2.putText(img, text, (x+1, y+1), cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 0), 2, cv2.CV_AA)
     cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), 1, cv2.CV_AA)
+    
+def smooth_face(rects):
+    global smooth_face_counter, smooth_face_history
+    
+    # choose which face rect to process
+    # currently the first one is choosen, even if incorrect
+    if len(rects) > 0:
+        # store first rect in history list
+        smooth_face_history[smooth_face_counter] = rects[0:1]
+        smooth_face_counter += 1
         
+        # reset counter, if necessary
+        if smooth_face_counter >= SMOOTH_FACE_MAX:
+            smooth_face_counter = 0
+    
+    x1sum = y1sum = x2sum = y2sum = 0
+    
+    for x1, y1, x2, y2 in smooth_face_history:
+        x1sum += x1
+        y1sum += y1
+        x2sum += x2
+        y2sum += y2
+    
+    return [np.array([int(x1sum/SMOOTH_FACE_MAX), int(y1sum/SMOOTH_FACE_MAX), int(x2sum/SMOOTH_FACE_MAX), int(y2sum/SMOOTH_FACE_MAX)])]
 
 if __name__ == '__main__':
     
     cascade = cv2.CascadeClassifier(cascade_fn)
     nested = cv2.CascadeClassifier(nested_fn)
     
-    cam = create_capture(-1)
+    cam = cv2.VideoCapture(-1)
     
     frame_read_time, face_detect_time, frames_per_second = 0, 0, 0
     display_frt, display_fdt, display_fps = 0, 0, 0
     counter = 0
     previous_display_time = 0
+    
+    color = 0
     
     while True:
         t1 = time.time()
@@ -74,9 +105,14 @@ if __name__ == '__main__':
         
         rects = detect_face(gray, cascade)
         vis = img.copy()
-        draw_rects(vis, rects, (0, 255, 0))
+        if SHOW_REAL_TIME:
+            draw_rects(vis, rects, (0, 255, 0))
         
-        subrects = []
+        if len(rects) > 1:
+            rect = rects[0:1]
+            print "---"
+            for item in rect:
+                print item
         
         for x1, y1, x2, y2 in rects:
             roi = gray[y1:y2, x1:x2]
@@ -86,13 +122,25 @@ if __name__ == '__main__':
         
         t3 = time.time()
         
+        if SHOW_SMOOTH:
+            if len(rects) > 0:
+                color += 16
+                if color > 255:
+                    color = 255
+            else:
+                color -= 16
+                if color < 0:
+                    color = 0
+                    
+            draw_rects(vis, smooth_face(rects), (0, color, (255-color)))
+        
+        t4 = time.time()
+        
         #----------------------------------------
         # General image processing done. The 
         # following is for demonstration and
         # debug.
         #----------------------------------------
-        
-        # print str(len(rects)) + " - " + str(len(subrects))
         
         counter = counter + 1
         frame_read_time += t2 - t1
@@ -110,10 +158,11 @@ if __name__ == '__main__':
             frame_read_time, face_detect_time, frames_per_second = 0, 0, 0
         
         # Draw stats
-        draw_text(vis, 'frame read time: %.1f ms' % display_frt, (20, 20))
-        draw_text(vis, 'face detect time: %.1f ms' % display_fdt, (20, 40))
-        draw_text(vis, 'fps: %.1f' % display_fps, (20, 60))
-        draw_text(vis, 'time: %.3f' % time.time(), (20, 80))
+        draw_text(vis, 'frame read time: %.1f ms' % (1000+display_frt), (20, 20))
+        draw_text(vis, 'face detect time: %.1f ms' % (1000+display_fdt), (20, 40))
+        draw_text(vis, 'fps: %.1f' % (1000+display_fps), (20, 60))
+        draw_text(vis, 'time: %.3f' % (1000+time.time()), (20, 80))
+        draw_text(vis, 'Total time: %.1f ms' % (1000+(1000*(t4-t1))), (20, 100))
         
         # Show images
         cv2.imshow('Grayscale', gray)
